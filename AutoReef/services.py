096a99ee02114edd5cc70f9e0c59b96a3927f56d
Stephen Mc Gowan <mcclown@gmail.com>
2018-01-05 23:17:13 +1100

    Fixing the exit that happens occasionally when a google sheets login fails

--- AutoReef/services.py
+++ AutoReef/services.py
@@ -51,7 +51,7 @@ def login_open_sheet(oauth_key_file, spreadsheet):
     except Exception as ex:
         print('Unable to login and get spreadsheet.  Check OAuth credentials, spreadsheet name, and make sure spreadsheet is shared to the client_email address in the OAuth .json file!')
         print('Google sheet login failed with error:', ex)
-        sys.exit(1)
+        #sys.exit(1)
 
 
 class LoggingService:
